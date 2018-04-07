4ce0000940e84bca97fbe88449055209bcfd9acb
Mayur Patil <ram.nath241089@gmail.com>
2018-04-07 15:54:36 +0530

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/sh
 echo Destination IP address of machine: 
 read dest_ip_addr
-echo Your Destination IP is $dest_ip_addr
-su -
-scp sys-soft.sh root@dest_ip_addr:/root && ssh root@dest_ip_addr
-chmod 755 sys-soft.sh
-./sys-soft.sh
+echo For File copying
+scp sys-soft.sh root@$dest_ip_addr:/root 
+echo For root access
+ssh root@$dest_ip_addr '/root/sys-soft.sh'

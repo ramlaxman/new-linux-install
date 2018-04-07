d44bb2f5138e069a8b5b387b2ccd4b5e9b41a8ca
Mayur Patil <ram.nath241089@gmail.com>
2018-04-07 15:36:36 +0530

    corrected scripts

--- ssh-check.sh
+++ ssh-check.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
-yum install openssh
+dnf install openssh
 service sshd start
 chkconfig sshd on
-#echo Do you want to continue? [Y/N]
-#read ans
+echo Do you want to continue? [Y/N]
+read ans
 
-#if [ $ans == 'N']
-#then
-	#echo SSH check completed. Have a nice day.
-#else
-source transfer.sh 
-#fi
+if [ $ans == 'N' ] 
+then
+	echo "SSH check completed. Have a nice day."
+else
+	/root/new-linux-install/transfer.sh
+fi

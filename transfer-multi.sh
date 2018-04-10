fd20c7f4ed2e09f5c9b0135bf22f58fff45e8b3e
Mayur Patil <ram.nath241089@gmail.com>
2018-04-10 15:50:35 +0530

    Update transfer-multi.sh

--- transfer-multi.sh
+++ transfer-multi.sh
@@ -4,6 +4,6 @@ do
 	echo "For file copying"	
 	scp sys-soft.sh "$server":/root/
 	echo "For SSH Connection"
-	ssh root@$server '/root/sys-soft.sh' -i /home/ubuntu/.ssh/id_rsa $1
+	ssh root@$server '/root/sys-soft.sh' -i /home/student/.ssh/id_rsa $1
 	echo "============================"
 done
